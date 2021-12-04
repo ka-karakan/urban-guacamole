885ae1b8c720c50aff5277e2ece958d2358b6d2b
Tomasz Jaciubek <tomek.jaciubek@elancoah.com>
2021-12-04 12:31:07 +0000

    Initial Commit

---
+++ Tomek.Jaciubek.Service1.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Tomek.Jaciubek.Service1.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Tomek.Jaciubek.Service2.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Tomek.Jaciubek.Service2.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
